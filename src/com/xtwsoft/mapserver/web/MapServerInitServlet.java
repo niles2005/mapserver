8c9e5891d58609aed3cd6c715f6e70543b740f51
zoboya <rayzang8@gmail.com>
2013-06-25 16:32:17 +0800

    update

--- src/com/xtwsoft/mapserver/web/MapServerInitServlet.java
+++ src/com/xtwsoft/mapserver/web/MapServerInitServlet.java
@@ -17,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 public class MapServerInitServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
