381d4d173e0d408b7f805795d6d3d036f0e7d947
niles <niles2005@gmail.com>
2013-06-25 11:12:09 +0800

    update

---
+++ src/com/xtwsoft/mapserver/web/ServerConfig.java
@@ -0,0 +1,26 @@
+package com.xtwsoft.mapserver.web;
+
+import java.io.File;
+
+public class ServerConfig {
+	private static ServerConfig m_instance = null;
+	private File m_appPath = null;
+	private File m_dataPath = null;
+	private File m_storePath = null;
+	private ServerConfig(File appPath) {
+		m_appPath = appPath;
+	}
+
+	public static ServerConfig getInstance() {
+		return m_instance;
+	}
+	
+	public static void initInstance(String path) {
+		if(m_instance != null) {
+			return;
+		}
+		
+		m_instance = new ServerConfig(new File(path)); 
+	}
+	
+}
