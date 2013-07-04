8cb279be79379b3f8a8c30c83ab3b83bf5bcac70
zoboya <rayzang8@gmail.com>
2013-07-04 09:55:41 +0800

    update

--- docs/buildDoc.js
+++ docs/buildDoc.js
@@ -1,3 +1,41 @@
+//var $ = require('jquery');
+//var jsdom = require('jsdom');
 var api = require('./api.json');
+var __ = require('underscore');
+var stringUtil = require('underscore.string');
 
-console.log(api)
+var fs = require('fs');
+var doc1 = fs.readFileSync('./part1.html',"utf-8");
+var doc2 = fs.readFileSync('./part2.html',"utf-8");
+var doc3 = fs.readFileSync('./part3.html',"utf-8");
+var part1 = doc1.split('\r\n');
+var part2 = doc2.slice('\r\n');
+var part3 = doc3.slice('\r\n');
+
+var items = api[0]["items"];
+var titlePart = [];
+titlePart.push("<ul class=\"toc_section\">");
+__.each(items,function(item) {
+    titlePart.push('<li>- <a href="#list">' + item.name + '</a></li>');
+});
+titlePart.push('</ul>');
+
+var contentPart = [];
+__.each(items,function(item){
+    contentPart.push('<p id="'+item.name +'">');
+    contentPart.push('<b class="header">'+item.name +'</b><code>'+item.code+'</code>');
+    contentPart.push('<br><span class="alias">Return: <b>'+item.JSON+'</b></span>');
+    contentPart.push('<pre>'+item.retcontent+'</pre>');
+    contentPart.push(item.info);
+    contentPart.push('</p><br>');
+});
+
+
+var total = part1.concat(titlePart).concat(part2).concat(contentPart).concat(part3);
+var newDoc = total.join("\r\n");
+
+console.log(newDoc);
+
+var fileWriteStream = fs.createWriteStream('./newDoc.html');
+fileWriteStream.write(newDoc);
+//newDoc.pipe(fileWriteStream);
